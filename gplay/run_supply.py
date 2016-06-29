e652814ac649cc25141847cb782319306ae8b872
Duc Nguyen <ndpduc@gmail.com>
2016-06-29 21:17:02 +0700

    - Change package name & api key file name.

--- gplay/run_supply.py
+++ gplay/run_supply.py
@@ -4,6 +4,6 @@
 import sys
 import os, os.path
 
-cmd = 'supply -j ../Developer-412b5ee99736.json -p com.senspark.pet.idle.clicker.games.hamster --skip_upload_apk true --skip_upload_images true --skip_upload_screenshots true'
+cmd = 'supply -j ../"Google Play Android Developer-412b5ee99736.json" -p com.senspark.pets.idle.clicker.games.hamster --skip_upload_apk true --skip_upload_images true --skip_upload_screenshots true'
 print cmd
 os.system(cmd)
